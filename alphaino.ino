ced21b850cf7cc24d8f59512dc914220aa8c28cd
yunsangq <yunsangq@gmail.com>
2016-08-15 19:48:36 +0900

    예선 최종코드

--- alphaino.ino
+++ alphaino.ino
@@ -27,13 +27,13 @@ int check = 0;
 void loop() {
   cnt++;
   //Serial.println(cnt);
-  if(cnt > 36500){
+  if(cnt > 35500){
     if((Robot.Front_IRread(0) > VALUE) && (Robot.Front_IRread(1) > VALUE) && (Robot.Front_IRread(2) > VALUE) && (Robot.Front_IRread(3) > VALUE) && (Robot.Front_IRread(4) > VALUE) && (Robot.Front_IRread(5) > VALUE) && (Robot.Rear_IRread(0) > VALUE) && (Robot.Rear_IRread(1) > VALUE) && (Robot.Rear_IRread(2) > VALUE) && (Robot.Rear_IRread(3) > VALUE) && (Robot.Rear_IRread(4) > VALUE) && (Robot.Rear_IRread(5) > VALUE)){
       Robot.motors(0,0);
       delay(5000);
     }
   }
-  else if(cnt < 23500 || check == 1){
+  else if(cnt < 22800 || check == 1){
     //우회전 방향전환 => |a| > |b| (a>0, b<0)
     // 0 0 0 0 1 1 AND
     if((Robot.Front_IRread(4) < VALUE) && (Robot.Front_IRread(5) < VALUE) && (Robot.Front_IRread(3) < VALUE)){
@@ -52,7 +52,7 @@ void loop() {
       delay(100);
     }
     
-    //직진 가능하도록 속도 200근처에서 좌 우 최대한 수평값 찾기(내일)
+    //직진
     // 0 0 1 1 0 0
     if((Robot.Front_IRread(2) < VALUE) || (Robot.Front_IRread(3) < VALUE)){
       Robot.motors(-165, -185);
@@ -70,8 +70,8 @@ void loop() {
       Robot.motors(-120, -30);
     }
   }
-  else if(cnt < 30000 && cnt >= 23500){
-    //직진 가능하도록 속도 200근처에서 좌 우 최대한 수평값 찾기(내일)
+  else if(cnt < 30000 && cnt >= 22800){
+    //직진
     // 0 0 1 1 0 0
     if((Robot.Front_IRread(2) < VALUE) || (Robot.Front_IRread(3) < VALUE)){
       Robot.motors(-160, -180);
@@ -100,7 +100,5 @@ void loop() {
         }
       } 
     }
-    
   }
-  //delay(20);
 }
