29bc8dd5543c0d34cb4f02ed3b9fb0cab00888ef
yunsangq <yunsangq@gmail.com>
2016-08-19 15:26:41 +0900

    대회 파이널(실패)

--- alphaino.ino
+++ alphaino.ino
@@ -3,7 +3,7 @@
 #include <Wire.h>
 #include <SPI.h>
 
-#define VALUE  500
+#define VALUE  400
 
 void setup() { 
   //Serial.begin(9600);
@@ -22,159 +22,140 @@ void setup() {
 // 왼쪽회전 -> R,L => a > b
 // 오른쪽회전 -> R,L => a < b
 long cnt=0;
+int check =0;
 void loop() {
   cnt++;
-  //코스에 따라 수정
+
   /*
   if((Robot.Front_IRread(0) < VALUE) && (Robot.Front_IRread(1) < VALUE) && (Robot.Front_IRread(2) < VALUE) && (Robot.Front_IRread(3) < VALUE) && (Robot.Front_IRread(4) < VALUE) && (Robot.Front_IRread(5) < VALUE)){
     Robot.motors(-230, -230);
     delay(500);
   }
-  */
+  *//*
+  //코스에 따라 수정
   if(cnt<20000){
     if((Robot.Front_IRread(0) < VALUE) && (Robot.Front_IRread(1) < VALUE) && (Robot.Front_IRread(2) < VALUE) && (Robot.Front_IRread(3) < VALUE) && (Robot.Front_IRread(4) < VALUE) && (Robot.Front_IRread(5) < VALUE)){
       Robot.motors(-230, -230);
+      //delay(500);
     }
-  }
-  
-  //우회전 방향전환 => |a| > |b| (a>0, b<0)
-  if((Robot.Front_IRread(4) < VALUE) && (Robot.Front_IRread(5) < VALUE) && (Robot.Front_IRread(3) < VALUE)){
-    Robot.motors(80, -150);
-    delay(200);
-  }
-  
-  //좌회전 방향전환 => |a| < |b| (a<0, b>0) 
-  else if((Robot.Front_IRread(0) < VALUE) && (Robot.Front_IRread(1) < VALUE) && (Robot.Front_IRread(2) < VALUE)){ 
-    Robot.motors(-150, 80);
-    delay(200);
-  }
-
-
-  if( (Robot.Front_IRread(2) < VALUE) || (Robot.Front_IRread(3) < VALUE) )
-  {
-      Robot.motors(-230, -230);
-  }
-  else if( (Robot.Front_IRread(0) < VALUE) || (Robot.Front_IRread(1) < VALUE) )
-  {
-      Robot.motors(-150, 80);
-  }
-  else if( (Robot.Front_IRread(4) < VALUE) || (Robot.Front_IRread(5) < VALUE) )
-  {
-      Robot.motors(80, -150);
-  }
-
-  //코스에 따라 수정
-  if( cnt>30000 && cnt<38000)
-  {
-    if( (Robot.Front_IRread(0) > VALUE) && (Robot.Front_IRread(1) > VALUE) && (Robot.Front_IRread(2) > VALUE) && (Robot.Front_IRread(3) > VALUE) && (Robot.Front_IRread(4) > VALUE) && (Robot.Front_IRread(5) > VALUE) )
-    {
-        Robot.motors(-130,-100);
-    }
-  }
+  }*/
 
   //정지
-  if( cnt>45000 )
+  if( cnt>370000 )
   {
+    
      if((Robot.Front_IRread(0) > VALUE) && (Robot.Front_IRread(1) > VALUE) && (Robot.Front_IRread(2) > VALUE) && (Robot.Front_IRread(3) > VALUE) && (Robot.Front_IRread(4) > VALUE) && (Robot.Front_IRread(5) > VALUE) && (Robot.Rear_IRread(0) > VALUE) && (Robot.Rear_IRread(1) > VALUE) && (Robot.Rear_IRread(2) > VALUE) && (Robot.Rear_IRread(3) > VALUE) && (Robot.Rear_IRread(4) > VALUE) && (Robot.Rear_IRread(5) > VALUE)){
         Robot.motors(0,0);
         delay(10000);
       }
+      
    }
-}
 
-/*
-#include <ArduinoRobot.h>
-#include <Wire.h>
-#include <SPI.h>
-
-#define VALUE  500
-
-void setup() { 
-  Serial.begin(9600);
-  Robot.begin();   
-  Robot.stop();    
-}
-
-long cnt = 0; 
-int check = 0;
-void loop() {
-  cnt++;
-  //Serial.println(cnt);
-  if(cnt > 35500){
-    if((Robot.Front_IRread(0) > VALUE) && (Robot.Front_IRread(1) > VALUE) && (Robot.Front_IRread(2) > VALUE) && (Robot.Front_IRread(3) > VALUE) && (Robot.Front_IRread(4) > VALUE) && (Robot.Front_IRread(5) > VALUE) && (Robot.Rear_IRread(0) > VALUE) && (Robot.Rear_IRread(1) > VALUE) && (Robot.Rear_IRread(2) > VALUE) && (Robot.Rear_IRread(3) > VALUE) && (Robot.Rear_IRread(4) > VALUE) && (Robot.Rear_IRread(5) > VALUE)){
-      Robot.motors(0,0);
-      delay(5000);
-    }
-  }
-  else if(cnt < 22800 || check == 1){
+  else if(check == 1){
     //우회전 방향전환 => |a| > |b| (a>0, b<0)
-    // 0 0 0 0 1 1 AND
     if((Robot.Front_IRread(4) < VALUE) && (Robot.Front_IRread(5) < VALUE) && (Robot.Front_IRread(3) < VALUE)){
-      //Robot.motors(0,0);
-      //delay(150);
-      Robot.motors(90, -120);
-      delay(300);
-      //delay(100);
+      Robot.motors(80, -150);
+      delay(200);
     }
     
     //좌회전 방향전환 => |a| < |b| (a<0, b>0) 
-    // 1 1 0 0 0 0 AND
-    if((Robot.Front_IRread(0) < VALUE) && (Robot.Front_IRread(1) < VALUE) && (Robot.Front_IRread(2) < VALUE)){ 
-      //Robot.motors(0,0);
-      //delay(150);
-      Robot.motors(-110, 100);
-      delay(300);
-      //delay(100);
+    else if((Robot.Front_IRread(0) < VALUE) && (Robot.Front_IRread(1) < VALUE) && (Robot.Front_IRread(2) < VALUE)){ 
+      Robot.motors(-150, 80);
+      delay(200);
+    }
+    else if( (Robot.Front_IRread(2) < VALUE) || (Robot.Front_IRread(3) < VALUE))
+    {
+        Robot.motors(-230, -230);
+    }
+    else if( (Robot.Front_IRread(0) < VALUE) || (Robot.Front_IRread(1) < VALUE))
+    {
+        Robot.motors(-150, 80);
+    }
+    else if( (Robot.Front_IRread(4) < VALUE) || (Robot.Front_IRread(5) < VALUE))
+    {
+        Robot.motors(80, -150);
+    }
+  
+    
+  }
+    else if(cnt < 90000){
+    
+    if( (Robot.Front_IRread(2) < VALUE) || (Robot.Front_IRread(3) < VALUE))
+    {
+        Robot.motors(-230, -230);
+    }
+    else if( (Robot.Front_IRread(0) < VALUE) || (Robot.Front_IRread(1) < VALUE))
+    {
+        Robot.motors(-150, 80);
+    }
+    else if( (Robot.Front_IRread(4) < VALUE) || (Robot.Front_IRread(5) < VALUE))
+    {
+        Robot.motors(80, -150);
+    }
+    //우회전 방향전환 => |a| > |b| (a>0, b<0)
+    else if((Robot.Front_IRread(4) < VALUE) && (Robot.Front_IRread(5) < VALUE) && (Robot.Front_IRread(3) < VALUE)){
+      Robot.motors(80, -150);
+      delay(200);
     }
     
-    //직진
-    // 0 0 1 1 0 0
-    if((Robot.Front_IRread(2) < VALUE) || (Robot.Front_IRread(3) < VALUE)){
-      Robot.motors(-210, -230);
+    //좌회전 방향전환 => |a| < |b| (a<0, b>0) 
+    else if((Robot.Front_IRread(0) < VALUE) && (Robot.Front_IRread(1) < VALUE) && (Robot.Front_IRread(2) < VALUE)){ 
+      Robot.motors(-150, 80);
+      delay(200);
     }
-
-    //우회전 직진 보정 => |a| < |b| (a<0, b<0)
-    // 0 0 0 0 1 1 OR
-    if((Robot.Front_IRread(4) < VALUE) || (Robot.Front_IRread(5) < VALUE)){
-      Robot.motors(-30, -130);
-    }
-
-    //좌회전 직진 보정 => |a| > |b| (a<0, b<0)
-    // 1 1 0 0 0 0 OR
-    if((Robot.Front_IRread(0) < VALUE) || (Robot.Front_IRread(1) < VALUE)){
-      Robot.motors(-120, -30);
-    }
+    
   }
-  else if(cnt < 30000 && cnt >= 22800){
+
+  //코스에 따라 수정
+  else if( cnt>=90000 && cnt<170000)
+  {
     //직진
     // 0 0 1 1 0 0
-    if((Robot.Front_IRread(2) < VALUE) || (Robot.Front_IRread(3) < VALUE)){
+    /*
+    if((Robot.Front_IRread(0) > VALUE) && (Robot.Front_IRread(1) > VALUE) && (Robot.Front_IRread(2) > VALUE) && (Robot.Front_IRread(3) > VALUE) && (Robot.Front_IRread(4) > VALUE) && (Robot.Front_IRread(5) > VALUE)){
+      Robot.motors(-160,-100);
+      check=1;
+    }
+    else if((Robot.Front_IRread(2) < VALUE) || (Robot.Front_IRread(3) < VALUE)){
       Robot.motors(-160, -180);
     }
 
     //우회전 직진 보정 => |a| < |b| (a<0, b<0)
     // 0 0 0 0 1 1 OR
-    if((Robot.Front_IRread(4) < VALUE) || (Robot.Front_IRread(5) < VALUE)){
+    else if((Robot.Front_IRread(4) < VALUE) || (Robot.Front_IRread(5) < VALUE)){
       Robot.motors(-30, -100);
     }
 
     //좌회전 직진 보정 => |a| > |b| (a<0, b<0)
     // 1 1 0 0 0 0 OR
-    if((Robot.Front_IRread(0) < VALUE) || (Robot.Front_IRread(1) < VALUE)){
+    else if((Robot.Front_IRread(0) < VALUE) || (Robot.Front_IRread(1) < VALUE)){
       Robot.motors(-100, -30);
     }
- 
-    if((Robot.Front_IRread(0) > VALUE) && (Robot.Front_IRread(1) > VALUE) && (Robot.Front_IRread(2) > VALUE) && (Robot.Front_IRread(3) > VALUE) && (Robot.Front_IRread(4) > VALUE) && (Robot.Front_IRread(5) > VALUE) && (Robot.Rear_IRread(0) > VALUE) && (Robot.Rear_IRread(1) > VALUE) && (Robot.Rear_IRread(2) > VALUE) && (Robot.Rear_IRread(3) > VALUE) && (Robot.Rear_IRread(4) > VALUE) && (Robot.Rear_IRread(5) > VALUE)){
-      Robot.motors(0,0);
-      delay(500);
-      Robot.motors(-80, -60);
-      while(1){
-        if((Robot.Front_IRread(0) < VALUE) || (Robot.Front_IRread(1) < VALUE)  || (Robot.Front_IRread(2) < VALUE) || (Robot.Front_IRread(3) < VALUE)  || (Robot.Front_IRread(4) < VALUE) || (Robot.Front_IRread(5) < VALUE)){
-          check = 1;
-          break;
-        }
-      } 
+    */
+    if((Robot.Front_IRread(0) > VALUE) && (Robot.Front_IRread(1) > VALUE) && (Robot.Front_IRread(2) > VALUE) && (Robot.Front_IRread(3) > VALUE) && (Robot.Front_IRread(4) > VALUE) && (Robot.Front_IRread(5) > VALUE)){
+      Robot.motors(-170,-100);
+      check=1;
     }
-  }
+    else if((Robot.Front_IRread(2) < VALUE) || (Robot.Front_IRread(3) < VALUE)){
+      Robot.motors(-230, -230);
+    }
+
+    //우회전 직진 보정 => |a| < |b| (a<0, b<0)
+    // 0 0 0 0 1 1 OR
+    else if((Robot.Front_IRread(4) < VALUE) || (Robot.Front_IRread(5) < VALUE)){
+      Robot.motors(80, -150);
+    }
+
+    //좌회전 직진 보정 => |a| > |b| (a<0, b<0)
+    // 1 1 0 0 0 0 OR
+    else if((Robot.Front_IRread(0) < VALUE) || (Robot.Front_IRread(1) < VALUE)){
+      Robot.motors(-150, 80);
+    }
+    
 }
-*/
+
+  
+
+
+  
+}
